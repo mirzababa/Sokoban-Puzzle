50d18fbf81b4f5f27d2eed4014ef1b5e78f10947
mirzababa <34629367+mirzababa@users.noreply.github.com>
2018-06-07 22:10:08 +0430

    value function in progress

--- Source.cpp
+++ Source.cpp
@@ -6,9 +6,11 @@ start time: May 24, 2018*/
 #include<vector>
 #include <fstream> //read from file
 #include <string> //getline()
-#include <algorithm>//find
+#include <algorithm>    // std::max
 
 using namespace std;
+
+bool reward_10 = false;
 
 struct agent
 {
@@ -145,77 +147,112 @@ vector<tile> makegrid()
 	return MovementEnvironmet;
 }
 
-vector<state>stategenerator(state s, vector<tile>grid , short int w) 
+/*Explanation: Getting free houses to move the agent in the game environment
+input: --
+output:The vector includes the houses other than walls of the game environment*/
+state stategenerator(state s, vector<tile>grid, short int w)
 {
 	state temp;
-	vector<state>newstates;
-	for (short int i = 0; i < grid.size(); i++) 
+	//up
+	if (w == 1)
 	{
-		if (grid[i].agent == true)
+		for (short int i = 0; i < grid.size(); i++)
 		{
-			//up
-			if (w == 1)
+			if (s.a.x == grid[i].p.x + 1 && s.a.y == grid[i].p.y)
 			{
-				if (s.a.x == grid[i].p.x - 1 && s.a.y == grid[i].p.y)
-				{ /////////dummy//////////////!
-					temp.a.x = s.a.x - 1;	temp.a.y = s.a.y;
-					if (grid[i].p.x - 1 == s.b.x && grid[i].p.y == s.b.y && s.b.x == grid[i].p.x - 1 && s.b.y == grid[i].p.y) //hit the box
-					{
-						temp.b.x = s.b.x - 1;	temp.b.y = s.b.y;
-					}
-					temp.b.x = s.b.x;	temp.b.y = s.b.y;
-					newstates.push_back(temp);
-				}
-			}
-			//down
-			if (w == 3)
-			{
-				if (s.a.x == grid[i].p.x + 1 && s.a.y == grid[i].p.y)
+				temp.a.x = s.a.x - 1;	temp.a.y = s.a.y;
+				temp.b.x = s.b.x;	temp.b.y = s.b.y;
+				if (temp.a.x == temp.b.x && temp.a.y == temp.b.y) //رفته رو خونه بغلیش که جعبه هست
 				{
-					temp.a.x = s.a.x + 1;	temp.a.y = s.a.y;
-					if (grid[i].p.x + 1 == s.b.x && grid[i].p.y == s.b.y && s.b.x == grid[i].p.x + 1 && s.b.y == grid[i].p.y)
+					for (short int j = 0; j < grid.size(); j++)
 					{
-						temp.b.x = s.b.x + 1;	temp.b.y = s.b.y;
+						if (temp.a.x == grid[i].p.x + 1 && temp.a.y == grid[i].p.y)
+						{
+							temp.b.x = temp.b.x - 1;	temp.b.y = temp.b.y;
+						}
+						return temp;
 					}
-					temp.b.x = s.b.x;	temp.b.y = s.b.y;
-					newstates.push_back(temp);
-				}
-			}
-			//left
-			if (w == 4)
-			{
-				if (s.a.x == grid[i].p.x && s.a.y == grid[i].p.y - 1)
-				{
-					temp.a.x = s.a.x;	temp.a.y = s.a.y - 1;
-					if (grid[i].p.x == s.b.x && grid[i].p.y - 1 == s.b.y && s.b.x == grid[i].p.x && s.b.y == grid[i].p.y - 1)
-					{
-						temp.b.x = s.b.x;	temp.b.y = s.b.y - 1;
-					}
-					temp.b.x = s.b.x;	temp.b.y = s.b.y;
-					newstates.push_back(temp);
-				}
-			}
-			//right
-			if (w == 2)
-			{
-				if (s.a.x == grid[i].p.x && s.a.y == grid[i].p.y + 1)
-				{
-					temp.a.x = s.a.x;	temp.a.y = s.a.y + 1;
-					if (grid[i].p.x == s.b.x && grid[i].p.y + 1 == s.b.y && s.b.x == grid[i].p.x && s.b.y == grid[i].p.y + 1)
-					{
-						temp.b.x = s.b.x;	temp.b.y = s.b.y + 1;
-					}
-					temp.b.x = s.b.x;	temp.b.y = s.b.y;
-					newstates.push_back(temp);
 				}
 			}
 		}
-		break;
 	}
-	return newstates;
+
+	//right
+	if (w == 2)
+	{
+		for (short int i = 0; i < grid.size(); i++)
+		{
+			if (s.a.x == grid[i].p.x && s.a.y == grid[i].p.y - 1)
+			{
+				temp.a.x = s.a.x;	temp.a.y = s.a.y + 1;
+				temp.b.x = s.b.x;	temp.b.y = s.b.y;
+				if (temp.a.x == temp.b.x && temp.a.y == temp.b.y) 
+				{
+					for (short int j = 0; j < grid.size(); j++)
+					{
+						if (temp.a.x == grid[i].p.x && temp.a.y == grid[i].p.y - 1)
+						{
+							temp.b.x = temp.b.x;	temp.b.y = temp.b.y + 1;
+						}
+						return temp;
+					}
+				}
+			}
+		}
+	}
+
+	//down
+	if (w == 3)
+	{
+		for (short int i = 0; i < grid.size(); i++)
+		{
+			if (s.a.x == grid[i].p.x - 1 && s.a.y == grid[i].p.y)
+			{
+				temp.a.x = s.a.x + 1;	temp.a.y = s.a.y;
+				temp.b.x = s.b.x;	temp.b.y = s.b.y;
+				if (temp.a.x == temp.b.x && temp.a.y == temp.b.y) 
+				{
+					for (short int j = 0; j < grid.size(); j++)
+					{
+						if (temp.a.x == grid[i].p.x - 1 && temp.a.y == grid[i].p.y)
+						{
+							temp.b.x = temp.b.x + 1;	temp.b.y = temp.b.y;
+						}
+						return temp;
+					}
+				}
+			}
+		}
+	}
+
+	//left
+	if (w == 4)
+	{
+		for (short int i = 0; i < grid.size(); i++)
+		{
+			if (s.a.x == grid[i].p.x && s.a.y == grid[i].p.y + 1)
+			{
+				temp.a.x = s.a.x;	temp.a.y = s.a.y - 1;
+				temp.b.x = s.b.x;	temp.b.y = s.b.y;
+				if (temp.a.x == temp.b.x && temp.a.y == temp.b.y) 
+				{
+					for (short int j = 0; j < grid.size(); j++)
+					{
+						if (temp.a.x == grid[i].p.x && temp.a.y == grid[i].p.y + 1)
+						{
+							temp.b.x = temp.b.x;	temp.b.y = temp.b.y - 1;
+						}
+						return temp;
+					}
+				}
+			}
+		}
+	}
 }
 
-
+/*Explanation: Getting free houses to move the agent in the game environment
+input: --
+output:The vector includes the houses other than walls of the game environment*/
 short int reward(state s, short int a , vector<tile>grid) 
 {
 	state newstate;
@@ -234,10 +271,15 @@ short int reward(state s, short int a , vector<tile>grid)
 				return 100;//reach to the goal
 			}
 
-			if (grid[i].p.x == newstate.a.x  && grid[i].p.y== newstate.a.y
-				|| grid[i].p.x == newstate.b.x && grid[i].p.y == newstate.b.y) { return -10; }
+			if (reward_10 == false && grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
+				|| grid[i].p.x == newstate.b.x && grid[i].p.y == newstate.b.y) {
+				reward_10 = true;
+				return -10;
+			}
 		}
 		if (newstate.a.x == s.b.x && newstate.a.y == s.b.y) { return 10; } //hit the box
+
+		else { return 0; }
 	}
 
 	if (a = 2) {
@@ -254,12 +296,15 @@ short int reward(state s, short int a , vector<tile>grid)
 				return 100;//reach to the goal
 			}
 
-			if (grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
+			if (reward_10 == false && grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
 				|| grid[i].p.x == newstate.b.x && grid[i].p.y == newstate.b.y) {
+				reward_10 = true;
 				return -10;
 			}
 		}
 		if (newstate.a.x == s.b.x && newstate.a.y == s.b.y) { return 10; } //hit the box
+
+		else { return 0; }
 	}
 
 	if (a = 3) {
@@ -276,12 +321,15 @@ short int reward(state s, short int a , vector<tile>grid)
 				return 100;//reach to the goal
 			}
 
-			if (grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
+			if (reward_10 == false && grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
 				|| grid[i].p.x == newstate.b.x && grid[i].p.y == newstate.b.y) {
+				reward_10 = true;
 				return -10;
 			}
 		}
 		if (newstate.a.x == s.b.x && newstate.a.y == s.b.y) { return 10; } //hit the box
+
+		else { return 0; }
 	}
 
 	if (a = 4) {
@@ -298,24 +346,83 @@ short int reward(state s, short int a , vector<tile>grid)
 				return 100;//reach to the goal
 			}
 
-			if (grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
+			if (reward_10 == false && grid[i].p.x == newstate.a.x  && grid[i].p.y == newstate.a.y
 				|| grid[i].p.x == newstate.b.x && grid[i].p.y == newstate.b.y) {
+				reward_10 = true;
 				return -10;
 			}
 		}
 		if (newstate.a.x == s.b.x && newstate.a.y == s.b.y) { return 10; } //hit the box
+
+		else { return 0; }
 	}
 
 	
 }
 
-vector<value> value_iteration() {
+/*Explanation: Getting free houses to move the agent in the game environment
+input: --
+output:The vector includes the houses other than walls of the game environment*/
+//a -> eta
+float value_iteration(state s, float a, vector<tile>grid) 
+{
+	bool ifconvergence = false; //to find out if convergence accured or not
+	float val;
+	vector <float> max;
+	short int iterations = 0;
+	vector<value> v;
+	value init; 
+	state temp;
+	vector<state> state; // output of state generator
 
+	state.push_back(s);
+	//initial value table
+	init.s = s;	init.v = 0.0;
+	v.push_back(init);
+	temp = s;
+	state.erase(state.begin());
+	for (short int i = 1; i <= 4; i++) {
+		state[i] = stategenerator(s, grid, i);
+		init.s = state[i];	init.v = 0.0;
+		v.push_back(init);
+	}
+	
+	while (ifconvergence == false)
+	{
+		iterations += 1;
+
+		for (short int i = 0; i < state.size(); i++) {  //for each state
+			for (short int j = 1; j <= 4; j++) { //for each action
+
+				max[j] = reward(temp, j, grid) + a * (value_iteration(state[i], a, grid));
+			}
+			val = *max_element(max.begin(), max.end());
+		}
+	}
+	return val;
 }
 
 int main()
 {
 	vector<tile> temp = makegrid();
+	float v;
+	state start;
+	start.a.x = start.b.x = -100;
+	for (short int i = 0; i < temp.size(); i++) {
+		if (temp[i].agent == true) {
+			start.a.x = temp[i].p.x;	start.a.y = temp[i].p.y;
+		}
+		if (temp[i].box == true) {
+			start.b.x = temp[i].p.x;	start.b.y = temp[i].p.y;
+		}
+		if (start.a.x > 0 && start.b.x > 0) { break; }
+	}
 
+	for (int i = 0; i < 10; i++) {
+		reward_10 = false;
+		v = value_iteration(start, 0.8, temp);
+	}
+	 
+	 
 	return 0;
 }
